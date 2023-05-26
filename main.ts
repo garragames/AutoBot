ded7fb6e17eb200d799d6a61b584817a4bf20565
Garragames <96148865+garragames@users.noreply.github.com>
2023-05-25 22:33:30 -0600

    Initial Commit

--- main.ts
+++ main.ts
@@ -1,1 +1,264 @@
+/*
+ * Auto Bot
+ *
+ * by Jorge Gonzalez Garcia <jorge@garragames.com>
+ *
+ * /
 
+/**
+ * Custom graphic block
+ */
+//% weight=100 color=#fa8f13 icon="\uf124" block="Auto Bot"
+//% groups=['Directions', 'Movements', 'Sensors', 'Logic']
+namespace autoBot {
+
+    const MIN_VEL = 150;  //   0 km/hr
+    const MAX_VEL = 1023;  // 100 km/hr
+
+    /**
+     * Setup variables, pins and enumerations
+     */
+
+    // Disable LED Matrix
+    led.enable(true)
+
+    // Stop motors
+    stop()
+
+    // Set initial speed
+    setSpeed(50)
+
+    // Motor Directions
+    export enum EnumDir {
+        //% block="forward"
+        //% block.loc.es-ES="adelante"
+        Forward = 1,
+        //% block="backward"
+        //% block.loc.es-ES="atrás"
+        Backward = 2,
+        //% block="left"
+        //% block.loc.es-ES="izquierda"
+        Left = 3,
+        //% block="right"
+        //% block.loc.es-ES="derecha"
+        Right = 4,
+        //% block="stop"
+        //% block.loc.es-ES="alto"
+        Stop = 0
+    }
+
+    // Set speed to both motors
+    function setSpeed(speed: number): void {
+        let s = speed * (MAX_VEL - MIN_VEL) / 100 + MIN_VEL; // Adjust to the min and max PWM values
+        // PWM para izquierdo
+        pins.analogWritePin(AnalogPin.P8, s)
+        // PWM para derecho
+        pins.analogWritePin(AnalogPin.P16, s)
+    }
+
+    //% blockId=senseAcelerometer
+    //% block="sense acelerometer"
+    //% block.loc.es-ES="detectar acelerómetro"
+    //% group="Sensors"
+    export function senseAcelerometer(): number {
+        return 0
+    }
+
+    //% blockId=senseDistance
+    //% block="sense distance"
+    //% block.loc.es-ES="detectar distancia"
+    //% group="Sensors"
+    export function senseDistance(): number {
+        return 0
+    }
+
+    /**
+     * Detects the line through the five infrared sensors in front of the AutoBot, and returns forward, backward, left, right or stop.
+     */
+    //% blockId=senseLine
+    //% block="sense line"
+    //% block.loc.es-ES="detectar línea"
+    //% group="Sensors"
+    export function senseLine(): EnumDir {
+        if (pins.digitalReadPin(DigitalPin.P0) == 1 && (pins.digitalReadPin(DigitalPin.P1) == 1 && (pins.digitalReadPin(DigitalPin.P2) == 0 && (pins.digitalReadPin(DigitalPin.P3) == 1 && pins.digitalReadPin(DigitalPin.P4) == 1)))) {
+            return EnumDir.Forward
+        } else
+            if (pins.digitalReadPin(DigitalPin.P0) == 1 && (pins.digitalReadPin(DigitalPin.P1) == 0 && (pins.digitalReadPin(DigitalPin.P2) == 1 && (pins.digitalReadPin(DigitalPin.P3) == 1 && pins.digitalReadPin(DigitalPin.P4) == 1)))) {
+                return EnumDir.Left
+            } else
+                if (pins.digitalReadPin(DigitalPin.P0) == 0 && (pins.digitalReadPin(DigitalPin.P1) == 1 && (pins.digitalReadPin(DigitalPin.P2) == 1 && (pins.digitalReadPin(DigitalPin.P3) == 1 && pins.digitalReadPin(DigitalPin.P4) == 1)))) {
+                    return EnumDir.Left
+                } else
+                    if (pins.digitalReadPin(DigitalPin.P0) == 1 && (pins.digitalReadPin(DigitalPin.P1) == 1 && (pins.digitalReadPin(DigitalPin.P2) == 1 && (pins.digitalReadPin(DigitalPin.P3) == 0 && pins.digitalReadPin(DigitalPin.P4) == 1)))) {
+                        return EnumDir.Right
+                    } else
+                        if (pins.digitalReadPin(DigitalPin.P0) == 1 && (pins.digitalReadPin(DigitalPin.P1) == 1 && (pins.digitalReadPin(DigitalPin.P2) == 1 && (pins.digitalReadPin(DigitalPin.P3) == 1 && pins.digitalReadPin(DigitalPin.P4) == 0)))) {
+                            return EnumDir.Right
+                        } else
+                            if (pins.digitalReadPin(DigitalPin.P0) == 1 && (pins.digitalReadPin(DigitalPin.P1) == 1 && (pins.digitalReadPin(DigitalPin.P2) == 1 && (pins.digitalReadPin(DigitalPin.P3) == 1 && pins.digitalReadPin(DigitalPin.P4) == 1)))) {
+                                return EnumDir.Stop
+                            }
+        return EnumDir.Stop // If no condition then Stop motors
+    }
+
+
+    /**
+     * Stop the autobot
+     */
+    //% blockId=stop
+    //% block="stop"
+    //% block.loc.es-ES="alto"
+    //% group="Movements"
+    export function stop(): void {
+        pins.digitalWritePin(DigitalPin.P12, 0)
+        pins.digitalWritePin(DigitalPin.P13, 0)
+        pins.digitalWritePin(DigitalPin.P14, 0)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+    }
+
+    /**
+     * Turn the autobot right at speed between 0 and 100 km/hr
+     * @param speed
+     */
+    //% blockId=right
+    //% block="turn right $speed km/hr"
+    //% block.loc.es-ES="derecha $speed km/hr"
+    //% group="Movements"
+    //% speed.min=0 speed.max=100
+    //% speed.defl=50
+    export function right(speed: number): void {
+        setSpeed(speed)
+        pins.digitalWritePin(DigitalPin.P12, 0)
+        pins.digitalWritePin(DigitalPin.P13, 0)
+        pins.digitalWritePin(DigitalPin.P14, 1)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+    }
+
+
+    /**
+     * Turn the autobot left at speed between 0 and 100 km/hr
+     * @param speed
+     */
+    //%blockId=left
+    //%block="turn left $speed km/hr"
+    //% block.loc.es-ES="izquierda $speed km/hr"
+    //% group="Movements"
+    //% speed.min=0 speed.max=100
+    //% speed.defl=50
+    export function left(speed: number): void {
+        setSpeed(speed)
+        pins.digitalWritePin(DigitalPin.P12, 0)
+        pins.digitalWritePin(DigitalPin.P13, 1)
+        pins.digitalWritePin(DigitalPin.P14, 0)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+    }
+
+    /**
+     * Moves the autobot backward at speed between 0 and 100 km/hr
+     * @param speed
+     */
+    //%blockId=backward
+    //%block="go backward $speed km/hr"
+    //%block.loc.es-ES="atrás $speed km/hr"
+    //% group="Movements"
+    //% speed.min=0 speed.max=100
+    //% speed.defl=50
+    export function backward
+        (speed: number): void {
+        setSpeed(speed)
+        pins.digitalWritePin(DigitalPin.P12, 1)
+        pins.digitalWritePin(DigitalPin.P13, 0)
+        pins.digitalWritePin(DigitalPin.P14, 0)
+        pins.digitalWritePin(DigitalPin.P15, 1)
+    }
+
+
+    /**
+     * Moves the autobot forward at speed between 0 and 100 km/hr
+     * @param speed
+     */
+    //%blockId=forward
+    //%block="go forward $speed km/hr"
+    //%block.loc.es-ES="adelante $speed km/hr"
+    //% group="Movements"
+    //% speed.min=0 speed.max=100
+    //% speed.defl=50
+    export function forward(speed: number): void {
+        setSpeed(speed)
+        pins.digitalWritePin(DigitalPin.P12, 0)
+        pins.digitalWritePin(DigitalPin.P13, 1)
+        pins.digitalWritePin(DigitalPin.P14, 1)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+    }
+
+    /**
+     * Direction constants
+     */
+
+    /**
+     * Returns the value of stop to compare with the value returned by the line sensor.
+     */
+    //% blockId=stopDir
+    //% block="stop"
+    //% block.loc.es-ES="alto"
+    //% group="Directions"
+    export function stopDir(): number {
+        return EnumDir.Stop
+    }
+
+    /**
+     * Returns the value of right to compare with the value returned by the line sensor.
+     */
+    //% blockId=rightDir
+    //% block="right"
+    //% block.loc.es-ES="derecha"
+    //% group="Directions"
+    export function rightDir(): number {
+        return EnumDir.Right
+    }
+
+
+    /**
+     * Returns the value of left to compare with the value returned by the line sensor.
+     */
+    //% blockId=leftDir
+    //% block="left"
+    //% block.loc.es-ES="izquierda"
+    //% group="Directions"
+    export function leftDir(): number {
+        return EnumDir.Left
+    }
+
+    /**
+     * Returns the value of backward to compare with the value returned by the line sensor.
+     */
+    //% blockId=backwardDir
+    //% block="backforward"
+    //% block.loc.es-ES="atrás"
+    //% group="Directions"
+    export function backwardDir(): number {
+        return EnumDir.Backward
+    }
+
+    /**
+     * Returns the value of forward to compare with the value returned by the line sensor.
+     */
+    //% blockId=forwardDir
+    //% block="forward"
+    //% block.loc.es-ES="adelante"
+    //% group="Directions"
+    export function forwardDir(): number {
+        return EnumDir.Forward
+    }
+
+    /**
+     * Compares two values and returns true if equal or false if not equal
+     */
+    //% blockId=compareDir
+    //% block="$directionA equal to $directionB"
+    //% block.loc.es-ES="$directionA igual a $directionB"
+    //% group="Logic"
+    export function equal(directionA: number, directionB: number): boolean {
+        return directionA == directionB
+    }
+}
--- test.ts
+++ test.ts
@@ -1,1 +1,21 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+/*
+ * Auto Bot Test
+ *
+ * by Jorge Gonzalez Garcia <jorge@garragames.com>
+ *
+ */
+
+basic.forever(function () {
+    if (autoBot.equal(autoBot.senseLine(), autoBot.forwardDir())) {
+        autoBot.forward(80)
+    }
+    if (autoBot.equal(autoBot.senseLine(), autoBot.leftDir())) {
+        autoBot.left(50)
+    }
+    if (autoBot.equal(autoBot.senseLine(), autoBot.rightDir())) {
+        autoBot.right(50)
+    }
+    if (autoBot.equal(autoBot.senseLine(), autoBot.stopDir())) {
+        autoBot.stop()
+    }
+})
