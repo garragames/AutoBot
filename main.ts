0e8ad440c2f370a79837023a91e5ef8c4f377694
Garragames <96148865+garragames@users.noreply.github.com>
2023-05-31 11:15:03 -0600

    Restore '30/5/2023, 21:46:47 Actualizar main.ts'

--- main.ts
+++ main.ts
@@ -96,7 +96,6 @@ namespace autoBot {
         //% jres=speeds.reverse
         Reverse = 20
     }
-    
     // Set speed to both motors
     function setSpeed(speed: number): void {
         let s = speed * (MAX_VEL - MIN_VEL) / 100 + MIN_VEL; // Adjust to the min and max PWM values
