6d68ddf6e439b7902ffc4047cee50a3958772149
Garragames <96148865+garragames@users.noreply.github.com>
2023-06-01 20:35:00 -0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -6,17 +6,20 @@
  */
 
 basic.forever(function () {
-    if (autoBot.equal(autoBot.senseLine(), autoBot.forwardDir())) {
-        autoBot.forward(80)
-    }
-    if (autoBot.equal(autoBot.senseLine(), autoBot.leftDir())) {
-        autoBot.left(50)
-    }
-    if (autoBot.equal(autoBot.senseLine(), autoBot.rightDir())) {
-        autoBot.right(50)
-    }
-    if (autoBot.equal(autoBot.senseLine(), autoBot.stopDir())) {
+    if (autoBot.equal(autoBot.senseLine(), autoBot.EnumDir.Stop)) {
         autoBot.stop()
+    } else {
+        if (autoBot.equal(autoBot.senseLine(), autoBot.EnumDir.Right)) {
+            autoBot.setSpeed3(autoBot.EnumSpeed.Second)
+            autoBot.turnDirection(autoBot.EnumSteeringwheel.Front)
+        } else {
+            if (autoBot.equal(autoBot.senseLine(), autoBot.EnumDir.Left)) {
+                autoBot.turnDirection(autoBot.EnumSteeringwheel.Left)
+            } else {
+                if (autoBot.equal(autoBot.senseLine(), autoBot.EnumDir.Forward)) {
+                    autoBot.turnDirection(autoBot.EnumSteeringwheel.Front)
+                }
+            }
+        }
     }
 })
-
