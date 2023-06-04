3823e3043f6307665865fc4e4fd64168bbbc1c84
Garragames <96148865+garragames@users.noreply.github.com>
2023-06-03 21:02:13 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -149,7 +149,7 @@ namespace autoBot {
      */
     //% blockId=setSeatbelt
     //% block="seat belt $status"
-    //% block.loc-es-ES="cinturón de seguridad $status"
+    //% block.loc.es-ES="cinturón de seguridad $status"
     //% status.defl=EnumSeatbelt.unfasten
     //% status.fieldEditor="imagedropdown"
     //% status.fieldOptions.columns=2
