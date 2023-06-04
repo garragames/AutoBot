1bb3d4f543ae607c751b854fb8cda94aa4e9967e
Garragames <96148865+garragames@users.noreply.github.com>
2023-06-03 20:35:33 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -34,7 +34,7 @@ namespace autoBot {
     export enum EnumSteeringwheel {
         //% block="left"
         //% block.loc.es-ES="izquierda"
-        //% jres=icons.seatbelt-unfasent
+        //% jres=icons.seatbelt-unfasten
         Left = 1,
         //% block="front"
         //% block.loc.es-ES="frente"
