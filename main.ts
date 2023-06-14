79582e938fda41896fb6b638bc43cc29e6c20e56
Garragames <96148865+garragames@users.noreply.github.com>
2023-06-14 15:08:16 -0600

    Actualizar main.ts

--- main.ts
+++ main.ts
@@ -297,9 +297,9 @@ namespace autoBot {
             MANUAL_GEAR = gear
             SPEED = gear
             if (gear == REVERSE_GEAR) {
-                FORWARD = false
-            } else {
                 FORWARD = true
+            } else {
+                FORWARD = false
             }
         } else {
             console.warn('You cannot shift gears if you do not have your seat belt fastened.')
